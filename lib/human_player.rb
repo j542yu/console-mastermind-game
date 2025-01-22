4c7c82bbc7120545382b6adf208b9480c0b81d48
judywsl <j542yu@uwaterloo.ca>
2025-01-22 12:33:34 -0500

    Refactor generate_code, add make_guess and to_s
     - getting human input originally in generate_code is
       now placed in ask_for_input method to reuse in make_guess
     - to_s allows correct conversion of human player to string
       especially in string interpolation

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,30 +1,45 @@
+# frozen_string_literal: true
+
+# Represents human player in Mastermind game
+#
+# This class handles taking human input
+# The player either creates a code and gives feedback on guesses as
+# a code giver, or guesses code until 12 turns have been exhausted
 class HumanPlayer
+  include AllowedColours
+
   def initialize
-    puts "Good greetings player, who are you?"
+    puts 'Good greetings player, who are you?'
     @name = gets.chomp
   end
 
   attr_reader :name
 
   def generate_code
-    puts "Alright #{name}, create a four-letter code using any combination of these colours:"
-    puts '(Y)ellow, (R)ed, (M)agenta, (B)lue, (G)reen, (P)urple'
-    print '=> '
-    
-    allowed_colours = ['Y', 'R', 'M', 'B', 'G', 'P']
-    code = gets.chomp.upcase.split('')
-    
-    is_valid_code = code.all? { |colour| allowed_colours.include?(colour) } && code.length == 4
-    
-    until (is_valid_code)
-      puts 'Invalid code! Please try again:'
+    puts "\nAlright #{name}, create a four-letter code using any combination of these colours:
+      #{AllowedColours::ALLOWED_COLOURS_FULL.join(', ')}\n"
+
+    ask_for_input
+  end
+
+  def make_guess
+    puts "\nMake your guess!"
+    ask_for_input
+  end
+
+  def ask_for_input
+    loop do
       print '=> '
       code = gets.chomp.upcase.split('')
 
-      is_valid_code = code.all? { |colour| allowed_colours.include?(colour) } && code.length == 4
+      is_valid_code = code.all? { |colour| AllowedColours::ALLOWED_COLOURS.include?(colour) } && code.length == 4
+      return code if is_valid_code
+
+      puts "\nInvalid input Your input must be four letters long and only include the given colours.\nTry again:"
     end
-    code
+  end
+
+  def to_s
+    name
   end
 end
-
-puts HumanPlayer.new.generate_code
