d1784a6814a721e4785454ca4b8e6e186a137358
judywsl <j542yu@uwaterloo.ca>
2025-01-22 12:37:50 -0500

    Add NumberOfRounds module and syntax polishing

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 module AllowedColours
-  ALLOWED_COLOURS = ['Y', 'R', 'M', 'B', 'G', 'P']  
+  ALLOWED_COLOURS_FULL = %w[(Y)ellow (R)ed (M)agenta (B)lue (G)reen (P)urple].freeze
+  ALLOWED_COLOURS = %w[Y R M B G P].freeze
+end
+
+module NumberOfRounds
+  MAX_ROUNDS = 12
 end
