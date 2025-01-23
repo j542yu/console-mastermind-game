c6a08364e06ecef839c565dd496e2a49a385a948
judywsl <j542yu@uwaterloo.ca>
2025-01-23 10:22:22 -0500

    Regroup constants into one module

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-module AllowedColours
-  ALLOWED_COLOURS_FULL = %w[(Y)ellow (R)ed (M)agenta (B)lue (G)reen (P)urple].freeze
-  ALLOWED_COLOURS = %w[Y R M B G P].freeze
-end
-
-module NumberOfRounds
+module GameConstants
+  COLOURS_FULL = %w[(Y)ellow (R)ed (M)agenta (B)lue (G)reen (P)urple].freeze
+  COLOURS = %w[Y R M B G P].freeze
   MAX_ROUNDS = 12
 end
