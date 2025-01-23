92f65270683abbc89d3ca98b45d1d4b06855b2d9
judywsl <j542yu@uwaterloo.ca>
2025-01-23 10:31:38 -0500

    Add game intro in main.rb
    Allows human to choose to either give code or
    guess code, or exit game if desired

--- main.rb
+++ main.rb
@@ -3,6 +3,24 @@
 require_relative 'lib/constants'
 require_relative 'lib/human_player'
 require_relative 'lib/computer_player'
-require_relative 'lib/game'
+require_relative 'lib/mastermind_game'
 
-Game.new(ComputerPlayer, HumanPlayer).play
+print "Welcome to Mastermind!\n\n"
+print 'In this game, one player creates a code and the '
+print "other player must guess based on the feedback given.\n"
+print "Would you like to be the code giver or code guesser? (giver/guesser)\n=> "
+
+human_player_role = gets.chomp
+
+until %w[giver guesser x].include?(human_player_role)
+  print "\nInvalid role. Please type either 'giver' or 'guesser', or 'x' to quit.\n=> "
+  human_player_role = gets.chomp
+end
+
+if human_player_role == 'giver'
+  MastermindGame.new(HumanPlayer, ComputerPlayer).play
+elsif human_player_role == 'guesser'
+  MastermindGame.new(ComputerPlayer, HumanPlayer).play
+else
+  puts "\nYou have exited the game. Goodbye traveler!"
+end
