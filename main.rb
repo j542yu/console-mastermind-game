2347b15d7c72c71a806b666adab289a5ce464efe
judywsl <j542yu@uwaterloo.ca>
2025-01-27 11:10:45 -0500

    Add colourful display using 'rainbow' gem

--- main.rb
+++ main.rb
@@ -1,26 +1,30 @@
 # frozen_string_literal: true
 
+require 'rainbow'
+
 require_relative 'lib/constants'
 require_relative 'lib/human_player'
 require_relative 'lib/computer_player'
 require_relative 'lib/game'
 
-print "Welcome to Mastermind!\n\n"
-print 'In this game, one player creates a code and the '
-print "other player must guess based on the feedback given.\n"
-print "Would you like to be the code giver or code guesser? (giver/guesser)\n=> "
+puts Rainbow("Welcome to Mastermind!\n").yellow.bright
+puts 'In this game, one player creates a code and the ' \
+'other player must guess based on the feedback given.'
+puts Rainbow('Would you like to (c)reate the code or (g)uess the code? (c/g)').darkblue.inverse
+print '=> '
 
-human_player_role = gets.chomp
+human_player_role = gets.chomp.downcase
 
-until %w[giver guesser x].include?(human_player_role)
-  print "\nInvalid role. Please type either 'giver' or 'guesser', or 'x' to quit.\n=> "
-  human_player_role = gets.chomp
+until %w[c g x].include?(human_player_role)
+  puts Rainbow("\nInvalid role. Please type either 'c' to create, 'g' to guess, or 'x' to quit.").red.inverse.underline
+  print '=> '
+  human_player_role = gets.chomp.downcase
 end
 
-if human_player_role == 'giver'
+if human_player_role == 'c'
   MastermindGame.new(HumanPlayer, ComputerPlayer).play
-elsif human_player_role == 'guesser'
+elsif human_player_role == 'g'
   MastermindGame.new(ComputerPlayer, HumanPlayer).play
 else
-  puts "\nYou have exited the game. Goodbye traveler!"
+  puts Rainbow("\nYou have exited the game. Goodbye traveler!").green
 end
